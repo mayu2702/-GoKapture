5fb45c00edf8fc6ef65b2a37cad236d48ba32a23
Mayur Bakal <131716114+mayu2702@users.noreply.github.com>
2024-08-25 23:41:01 +0530

    Create user.java

---
+++ user.java
@@ -0,0 +1,23 @@
+package com.example.taskmanagement.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+}
